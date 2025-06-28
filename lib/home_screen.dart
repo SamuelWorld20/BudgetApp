96dffc213aed9e12855474c9e7d1b6f097132b98
Samuel_Oluwaseyi <ejalonibuoluwaseyi@gmail.com>
2025-06-28 11:01:25 +0000

    flutter_launcher_icons

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -158,7 +158,7 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: const Icon(
           Icons.account_balance_wallet,
-          size: 20,
+          size: 50,
           // color: Colors.white.withOpacity(0.9),
           shadows: const [
             Shadow(
